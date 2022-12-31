9c83aa6cbb0f52f1ee124214015957dcd688a76b
Ari Archer <ari.web.xyz@gmail.com>
2022-12-31 10:01:35 +0200

    improve css and paste subapp, split it up
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- src/homeserver/app_paste.py
+++ src/homeserver/app_paste.py
@@ -5,24 +5,19 @@
 from typing import Any
 
 import sqlalchemy  # type: ignore
-from flask import (Blueprint, make_response, redirect, render_template,
-                   request, url_for)
+from flask import Blueprint, redirect, render_template, request, url_for
 from sqlalchemy.ext.declarative import declarative_base  # type: ignore
 from werkzeug.wrappers.response import Response
 
-from . import config
-from .subapp_res import paste as paste_res
+from . import util
+from .subapp_res import paste_res
 
 paste: Blueprint = Blueprint("paste", __name__)
 DB: dict[str, Any] = {}
 
 
-def get_paste_by_tid(tid: str) -> str | None:
-    return DB["session"].query(DB["paste"].content).filter_by(tid=tid).first()
-
-
 def prep_paste(_) -> None:
-    DB["engine"] = sqlalchemy.create_engine(f"sqlite:///{config.DB_DIR}/paste.db?check_same_thread=False")
+    DB["engine"] = sqlalchemy.create_engine(util.sqlite_db_path("pastes"))  # type: ignore
     DB["base"] = declarative_base()
 
     class Paste(DB["base"]):  # type: ignore
@@ -43,6 +38,15 @@ def prep_paste(_) -> None:
     DB["session"] = sqlalchemy.orm.Session(DB["engine"])  # type: ignore
 
     DB["paste"] = Paste
+
+
+def get_paste_by_tid(tid: str) -> str | None:
+    return DB["session"].query(DB["paste"].content).filter_by(tid=tid).first()
+
+
+@paste.get("/")
+def index() -> str:
+    return render_template("paste.j2")
 
 
 @paste.post("/")
@@ -71,19 +75,13 @@ def paste_content() -> Response:
     return redirect(url_for("paste.index") + tid)
 
 
-@paste.get("/")
-def index() -> str:
-    return render_template("paste.j2")
-
-
 @paste.get("/<tid>")
 def get_paste(tid: str) -> Response:
-    if (paste := get_paste_by_tid(tid)) is not None:
-        r: Response = make_response(paste[0], 200)
-        r.mimetype = "text/plain"
-        return r
-
-    return redirect(url_for("paste.index"), 404)
+    return (
+        util.redirect_view("paste.index", 404)
+        if (paste := get_paste_by_tid(tid)) is None
+        else util.plain_text_resp(paste[0])
+    )
 
 
 @paste.get("/<tid>/delete")
@@ -91,17 +89,20 @@ def delete_paste(tid: str) -> Response:
     status: int = 404
 
     if get_paste_by_tid(tid) is not None:
-        DB["session"].execute(
-            sqlalchemy.delete(DB["paste"]).where(DB["paste"].tid == tid)
+        util.exec_commit(  # type: ignore
+            DB["session"],
+            sqlalchemy.delete(DB["paste"]).where(DB["paste"].tid == tid),
         )
-        DB["session"].commit()
         status = 302
 
-    return redirect(url_for("paste.index"), status)
+    return util.redirect_view("paste.index", status)
 
 
 @paste.get("/list")
 def list_pastes() -> str:
     return render_template(
-        "list-pastes.j2", pastes=DB["session"].execute(f"SELECT * FROM {DB['paste'].__tablename__!r}"),
+        "list-pastes.j2",
+        pastes=tuple(
+            DB["session"].execute(f"SELECT * FROM {DB['paste'].__tablename__!r}")
+        ),
     )
---
+++ src/homeserver/util.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""utilities for subapps"""
+
+from typing import Any
+
+import sqlalchemy
+from flask import (Blueprint, make_response, redirect, render_template,
+                   request, url_for)
+from werkzeug.wrappers.response import Response
+
+from . import config
+
+
+def sqlite_db_path(name: str) -> str:
+    """returns a nice url for an sqlite3 db"""
+
+    return f"sqlite:///{config.DB_DIR}/{name}.db?check_same_thread=False"
+
+
+def plain_text_resp(text: str) -> Response:
+    """returns a plain-text-only response"""
+
+    r: Response = make_response(text, 200)
+    r.mimetype = "text/plain"
+    return r
+
+
+def redirect_view(slug: str, code: int = 302) -> Response:
+    """redirect to view rather than to a URL"""
+
+    return redirect(url_for(slug), code=code)
+
+
+def exec_commit(
+    session: sqlalchemy.orm.Session,  # type: ignore
+    query: Any,
+) -> None:
+    """execute a query on an SQL database and commit after"""
+
+    session.execute(query)
+    session.commit()
